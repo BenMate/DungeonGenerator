e108ceff4a6d22d4558e79b820b5b1b9c65c63c8
BenMate <benscott98@hotmail.com>
2022-05-17 10:22:42 +1000

    saving work

--- Assets/Scripts/BSPNode.cs
+++ Assets/Scripts/BSPNode.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Drawing;
 
 public class BSPNode
 {
@@ -15,33 +16,35 @@ public class BSPNode
     public Vector2 roomPos;
 
     GameObject box;
+
+    int minNodeSize = 50;
+
+    Rectangle room;
  
     //default Constructor
-    public BSPNode(int a_x, int a_y, int a_width, int a_height, int a_scale)
+    public BSPNode(int a_x, int a_y, int a_width, int a_height)
     {
-        //x,y,z position
+        //x,y position
         x = a_x;
         y = a_y;
        
-        
         width = a_width;
         height = a_height;
-        scale = a_scale;
-
+       
         hasRoom = false;
 
         box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        box.transform.position = new Vector3(x + (width * scale), y + (height * scale), 0.0f); //might not work ;c
+        box.transform.position = new Vector3(x + (width * 0.5f), y + (height * 0.5f), 0.0f);
         box.transform.localScale = new Vector3(width, height, 1.0f);
 
         //creates a random colour
-        Color randColor = new Color(Random.Range(0, 1.0f),
+        UnityEngine.Color randColor = new UnityEngine.Color(Random.Range(0, 1.0f),
             Random.Range(0, 1.0f), Random.Range(0, 1.0f), 1.0f);
         box.GetComponent<Renderer>().material.color = randColor;
         
     }
 
-    public bool BSPNode()
+    public bool BSPSplit()
     {
         //if leaf already has children skip over
         if (leftNode != null || rightNode != null)
@@ -58,10 +61,105 @@ public class BSPNode
             horizontal = true;
 
         //determine the max size of the new leaf
-        int max = (horizontal ? height : width) - min
+        int max = (horizontal ? height : width) - minNodeSize;
 
-            https://www.reddit.com/r/Unity3D/comments/2kjh1p/bsp_and_procedural_generation/clmaagr/
+        //if its to small, dont split
+        if (max <= minNodeSize)
+            return false;
+
+        //generate a new dimension for the split
+        int split = (int)Random.Range(minNodeSize, max);
+
+        //split
+        if (horizontal)
+        {
+            leftNode = new BSPNode(x, y, width, split);
+            rightNode = new BSPNode(x, y + split, width, height - split);
+
+        }
+        else //vertical
+        {
+            leftNode = new BSPNode(x, y, split, height);
+            rightNode = new BSPNode(x + split, y, width - split, height);
+        }
+
+        if (box != null)
+        {
+            GameObject.Destroy(box);
+            box = null;
+        }
+
+        //return true if split happened   
+        return true;
+    }
+
+    public void GenerateRoom()
+    {
+        if (leftNode != null || rightNode != null)
+        {
+            //checks left node
+            if (leftNode != null)
+                leftNode.GenerateRoom();
+
+            //checks right node
+            if (rightNode != null)
+                rightNode.GenerateRoom();
+
+            hasRoom = false;
+        }
+        else
+        {
+            roomSize = new Vector2(Random.Range(3, width - 2), Random.Range(3, height - 2));
+            roomPos = new Vector2(Random.Range(2, width - roomSize.x - 2), Random.Range(2, height - roomSize.y - 2));
+
+            var room = new Rectangle((int)(x + roomPos.x), (int)(y + roomPos.y), (int)roomSize.x, (int)roomSize.y);
+            hasRoom = false;
+        }
 
     }
+
+    public Rectangle GetRoom()
+    {
+        //iterate through the node to find a room, if one exists
+
+        if (room != null)
+            return room;
+
+        else
+        {
+            Rectangle lRoom;
+            Rectangle rRoom;
+
+            if (leftNode != null)
+                lRoom = leftNode.GetRoom();
+
+            if (rightNode != null)
+                rRoom = rightNode.GetRoom();
+
+            if (lRoom == null && rRoom == null)         
+                Debug.Log("should have returned null");// return null;
+                                                      
+            else if (rRoom == null)
+                return lRoom;
+
+            else if (lRoom == null)
+                return rRoom;
+
+            
+
+            
+
+            
+            
+
+            
+        }
+
+
+
+        return room;
+    }
+
+
 
 }
--- Assets/Scripts/ProceduralDungeonGen.cs
+++ Assets/Scripts/ProceduralDungeonGen.cs
@@ -6,40 +6,67 @@ using System.Linq;
 public class ProceduralDungeonGen : MonoBehaviour
 {
 
-    public int dungeonWidth, dungeonLength;
-    public int roomsWidthMin, roomsLengthMin;
-    public int maxIterations = 10;
-    public int hallwayWidth;
-    public Material floorMaterial;
-
-    public float roomBottomCornerOffSet;
-    public float roomTopCornerOffSet;
-    public int roomOffSet;
+    public int width = 400;
+    public int height = 400;
 
     BSPNode root;
+    public const int max_node_size = 50;
+    List<BSPNode> nodes = new List<BSPNode>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    public void CreateBSPTree()
-    {
-       //leaf 
-
-        
+        CreateRooms();
 
 
     }
 
     void CreateRooms()
     {
-        
+        root = new BSPNode(0, 0, width, height);
+        nodes.Add(root);
 
+        bool didSplit = true;
+
+        //we loop through every node in the vector.
+        while (didSplit)
+        {
+            didSplit = false;
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                //if left or right nodes is null
+                if (nodes[i].leftNode == null && nodes[i].rightNode == null)
+                {
+                    //if the node is to big
+                    if (nodes[i].width > max_node_size || nodes[i].height > max_node_size)
+                    {
+                        //if this node has not already been split
+                        if (nodes[i].BSPSplit())
+                        {
+                            //if we have did split, add child nodes
+                            nodes.Add(nodes[i].leftNode);
+                            nodes.Add(nodes[i].rightNode);
+                            didSplit = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        root.GenerateRoom();
     }
 
-   
+
 
 
 }
+
+
+
+
+
+
+
+
+
